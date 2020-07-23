52fc5f60689ab5941cc398d24aa86bd61ec7267d
ZACH HE (ITA-ISDC-ISD-OOCLL/ZHA) <HEZA2@OOCL.COM>
2020-07-23 12:37:25 +0800

    [zach] feature:given 3 play fizzbuzzwhizz return fizz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,6 +2,9 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     public String playFizzBuzzWhizz(int number){
+        if(number % 3 == 0){
+            return "fizz";
+        }
         return String.valueOf(number);
     }
 
